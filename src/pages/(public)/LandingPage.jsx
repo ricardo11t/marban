2ef7fff6fdb80a1f4647d4998eafaa4d239d736b
ricardo <jojotas1234@gmail.com>
2025-05-31 21:18:20 -0300

    acho que agora funciona

--- src/pages/(public)/LandingPage.jsx
+++ src/pages/(public)/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import Header from '.../components/Header'
+import Footer from '.../components/Footer'
 import { Button } from '@mui/material'
 import { Link } from 'react-router-dom'
 
